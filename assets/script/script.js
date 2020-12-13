43ca62668a00a761592ce83900246cc2ee41a66c
Amita <panwar.amita@gmail.com>
2020-12-13 02:25:55 -0500

    updated the syntax in index.html

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,10 +6,10 @@ var rightWrong = document.querySelector("#ans");
 var finalScore = 0;
 var finalResult = document.querySelector("#finalSore");
 var display = document.querySelector("#finalResult");
-var timer=document.querySelector("#count");
-var submitInitial=document.querySelector("#submit");
+var timer = document.querySelector("#count");
+var submitInitial = document.querySelector("#submit");
 //var buttons=document.querySelector("#buttons");
-   
+
 const myQuestions = [
     {
         question: "Who invented JavaScript?",
@@ -40,7 +40,7 @@ const myQuestions = [
         correctAnswer: "3"
     },
     {
-    question: "Which is not a primitive data type in JavaScript?",
+        question: "Which is not a primitive data type in JavaScript?",
         answers: {
             1: "boolean",
             2: "number",
@@ -51,75 +51,71 @@ const myQuestions = [
     },
     {
         question: "Which of these is a correct method to create a new array?",
-            answers: {
-                1: "var myArray = ();",
-                2: " var myArray = [];",
-                3: "var myArray = new Array[];"
-            },
-            correctAnswer: "2"
+        answers: {
+            1: "var myArray = ();",
+            2: " var myArray = [];",
+            3: "var myArray = new Array[];"
         },
-        {
-            question: "Which of these is not a logical operator?",
-                answers: {
-                    1: "!",
-                    2: "&",
-                    3: "&&",
-                    4: "||"
-                },
-                correctAnswer: "2"
-            }
+        correctAnswer: "2"
+    },
+    {
+        question: "Which of these is not a logical operator?",
+        answers: {
+            1: "!",
+            2: "&",
+            3: "&&",
+            4: "||"
+        },
+        correctAnswer: "2"
+    }
 ];
-startBt.addEventListener("click", function()
-{
+startBt.addEventListener("click", function () {
     setTimer();
     showQuesions();
 });
-var time=75;
-function setTimer()
-{
- //   event.preventDefault();
-    var interval=setInterval(() => {
-       
-        
-        if(time<=0)
-        {
+var time = 75;
+function setTimer() {
+    //   event.preventDefault();
+    var interval = setInterval(() => {
+
+
+        if (time <= 0) {
             clearInterval(interval);
             quizContainer.innerHTML = " ";
             rightWrong.innerHTML = "";
             display.style.display = "block";
-       // alert("completes");
-        //rightWrong.innerHTML = "";
-        finalResult.textContent = finalScore+".";
-        timer.textContent=time;
-        
+            // alert("completes");
+            //rightWrong.innerHTML = "";
+            finalResult.textContent = finalScore + ".";
+            timer.textContent = time;
+
         }
-        else
-        {
-           // console.log(time)
-            timer.textContent=time;
+        else {
+            // console.log(time)
+            timer.textContent = time;
             time--;
-           // showQuesions();
+            // showQuesions();
         }
-    },1000);
+    }, 1000);
 }
 var i = 0;
 function showQuesions() {
-   // event.preventDefault();
-   //location.href="score.html";
-   contentAlign.style.display="none";
-   //setTimer();
-   
+    // event.preventDefault();
+    //location.href="score.html";
+    contentAlign.style.display = "none";
+    //setTimer();
+
     var output = [];
     var answers = [];
 
     if (i > myQuestions.length - 1) {
-       // rightWrong.setAttribute("margin-top","300px");
+        // rightWrong.setAttribute("margin-top","300px");
         display.style.display = "block";
-       // alert("completes");
+        // alert("completes");
         //rightWrong.innerHTML = "";
-        finalResult.textContent = finalScore+".";
-        time=0;
-     
+        finalResult.textContent = finalScore + ".";
+        time = 0;
+
     }
     else {
 
@@ -145,10 +141,9 @@ function showQuesions() {
 
 }
 
-quizContainer.addEventListener("click", function(event){
-    if(event.target.nodeName==="LABEL")
-    {
-     
+quizContainer.addEventListener("click", function (event) {
+    if (event.target.nodeName === "LABEL") {
+
         goToNextQuestion(event);
     }
 });
@@ -160,25 +155,23 @@ function goToNextQuestion(event) {
         finalScore = finalScore + 5;
         correct = "<hr><label>Correct!</label>";
         rightWrong.innerHTML = correct;
-        var display=setTimeout(() => {
+        var display = setTimeout(() => {
             rightWrong.innerHTML = "";
         }, 2000);
-       
+
     }
     else {
-        time=time-10;
-        console.log("mintime:",time);
-        if(finalScore<=0)
-        {
-            finalScore=0;
+        time = time - 10;
+        console.log("mintime:", time);
+        if (finalScore <= 0) {
+            finalScore = 0;
         }
-        else
-        {
-        finalScore = finalScore - 5;
+        else {
+            finalScore = finalScore - 5;
         }
         correct = "<hr><label>Wrong!</label>";
         rightWrong.innerHTML = correct;
-        var display=setTimeout(() => {
+        var display = setTimeout(() => {
             rightWrong.innerHTML = "";
         }, 2000);
     }
@@ -187,18 +180,18 @@ function goToNextQuestion(event) {
     showQuesions();
     //setTimer();
 }
-var initial=document.querySelector("#initial");
-submitInitial.addEventListener("click", function(event){
+var initial = document.querySelector("#initial");
+submitInitial.addEventListener("click", function (event) {
     event.preventDefault();
 
-        if (initial.value === "")  {
-            alert("Please enter your initials!");
-            return null;
-        }
+    if (initial.value === "") {
+        alert("Please enter your initials!");
+        return null;
+    }
 
-        getHighScore(initial.value, finalScore);
-        window.location = "score.html";
-    });
+    getHighScore(initial.value, finalScore);
+    window.location = "score.html";
+});
 /*var people={
     initials:[],
     scores:[],
